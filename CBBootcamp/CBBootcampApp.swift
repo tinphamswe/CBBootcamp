2b32b6daed9c1fa127b1dfc78d758da3a849f3e9
Tin Pham <tinpham.swe@gmail.com>
2026-09-17 21:47:58 +0700

    Initial Commit

---
+++ CBBootcamp/CBBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CBBootcampApp.swift
+//  CBBootcamp
+//
+//  Created by Tin Pham on 17/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CBBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
